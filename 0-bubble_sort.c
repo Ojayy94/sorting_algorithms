0aebc0aa774db697a19d4afc572241bb15986e09
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-01-18 17:15:53 -0800

    sorting algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,10 @@
+#include <sort.h>
+
+/**
+ * bubble_sort - using the Bubble sort algorithm
+ * @array: print the array after each time you swap two elements 
+ * @size: Size
+ * Return: Sorted
+ */
+
+void bubble_sort(int *array, size_t size)
