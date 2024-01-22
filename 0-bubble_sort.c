004de07350456640e051310b14cb290581e15037
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-01-22 03:34:14 -0800

    bubbly sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,3 +8,29 @@
  */
 
 void bubble_sort(int *array, size_t size)
+{
+	int i;
+	int start = 0;
+	int max = 0;
+	int bubbly = 1;
+
+	if (array == NULL || size < 2)
+		return;
+
+	max = size - 1;
+
+	for (i = 0; i < max; ++i)
+	{
+		if (array[i] > array[i + 1])
+		{
+			start = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = start;
+			bubbly = 1;
+			print_array(array, size);
+		}
+
+		if (bubbly == 1 && i == max - 1)
+			i = -1, bubbly = 0, --max;
+	}
+}
